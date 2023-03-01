e2d4b201ffdd827a8d5e38c221cd21cbdfad9842
Devin Fritz <devinfritz@gmx.de>
2023-03-01 19:20:36 +0100

    Format

--- src/gifconverter.js
+++ src/gifconverter.js
@@ -1,30 +1,42 @@
-const GIFEncoder = require('gifencoder');
-const pngFileStream = require('png-file-stream');
+const GIFEncoder = require('gif-encoder-2')
+const pngFileStream = require('png-file-stream')
 const fs = require('fs');
-const fsextra = require('fs-extra');
+const fsextra = require('fs-extra')
 
 class GIFConverter {
-	constructor(width, heigth) {
-		this.gifencoder = new GIFEncoder(width, heigth);
-	}
 
-	createGif(inPath, out, pictures, deleteDir) {
-		const stream = pngFileStream(inPath + '/frame*.png')
-			.pipe(
-				this.gifencoder.createWriteStream({
-					repeat: 1,
-					delay: 10,
-					quality: pictures,
-				}),
-			)
-			.pipe(fs.createWriteStream(out));
+    constructor(width, heigth) {
+        this.gifencoder = new GIFEncoder(width, heigth);
+    }
 
-		stream.on('finish', () => {
-			if (deleteDir) fsextra.emptyDirSync('images');
+    async convertToGIF(outputPath, imageDataArray, width, height, delay = 100, repeat) {
+        console.debug("Convert images into .gif");
+        const encoder = new GIFEncoder(width, height);
+          
+        encoder.createReadStream().pipe(fs.createWriteStream(outputPath));
+        
+        encoder.start();
+        encoder.setDelay(delay);
+        encoder.setRepeat(repeat);
+        
+        for (frame in imageDataArray) {
+            encoder.addFrame(frame);
+        }
 
-			console.log('GIF Done');
-		});
-	}
+        encoder.finish();
+
+        const fileStream = fs.createWriteStream(outputPath);
+        stream.pipe(fileStream);
+        
+        return new Promise((resolve, reject) => {
+          fileStream.on('finish', () => {
+            resolve();
+          });
+          fileStream.on('error', (error) => {
+            reject(error);
+          });
+        });
+    }
 }
 
 module.exports = { GIFConverter };
