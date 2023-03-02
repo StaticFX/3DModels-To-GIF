7faaa973263ddad7a7f04d3d0a5daab48719d8c2
DerPenz <mike2407@t-online.de>
2023-03-02 19:56:00 +0100

    added objLoader Class

---
+++ src/loader/objLoader.js
@@ -0,0 +1,27 @@
+const fs = require('fs').promises;
+/**
+ * Wrapper Class to load .obj files into the scene
+ */
+class ObjLoader {
+	constructor() {}
+
+	/**
+	 * Loads a .obj file from a given filepath
+	 * @param {string} filepath as an absolute path
+	 * @returns {THREE.Group} group of loaded objects which can be added to the scene
+	 */
+	async load(filepath) {
+		const { OBJLoader } = await import(
+			'three/examples/jsm/loaders/OBJLoader.js'
+		);
+		this.objLoader = new OBJLoader();
+
+		const data = await fs.readFile(filepath, 'utf8');
+
+		const objects = this.objLoader.parse(data);
+
+		return objects;
+	}
+}
+
+module.exports = { ObjLoader };
