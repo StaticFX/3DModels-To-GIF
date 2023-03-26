af94bba78af59fa4185fe8941062438e58597118
DerPenz <mike2407@t-online.de>
2023-03-26 14:00:14 +0200

    added middleware

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ const express = require('express');
 const { createGifRouter } = require('./endpoints/createGifEndpoint.js');
 const { createTokenRouter } = require('./endpoints/createTokenEndpoint.js');
 const { statusRouter } = require('./endpoints/checkStatusEndpoint.js');
+const { errorHandlingMiddleWare } = require('./middleware/errorHandling.js');
 
 const app = express();
 
 app.use(express.json());
+
 app.use('/create', createGifRouter);
 app.use('/token', createTokenRouter);
 app.use('/check', statusRouter);
+
+app.use(errorHandlingMiddleWare);
 
 app.listen(process.env.PORT, () =>
 	console.log(`Listening on port ${process.env.PORT}`),
