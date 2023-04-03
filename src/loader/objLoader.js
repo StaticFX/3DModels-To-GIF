db3aac70f8eed9109dde830024caeee8ba8cccd4
DerPenz <mike2407@t-online.de>
2023-04-03 19:50:54 +0200

    abstracted all loading and spawning objects functionality into the specific loader

--- src/loader/objLoader.js
+++ src/loader/objLoader.js
@@ -4,32 +4,71 @@ const THREE = require('three');
 /**
  * Wrapper Class to load .obj files into the scene
  * @extends Loader
+ * @inheritdoc
  */
 class ObjLoader extends Loader {
+	/**
+	 * OBJ loader
+	 */
+	#objLoader;
+
+	/**
+	 * function to merge geometries
+	 */
+	#mergeBufferGeometries;
+
 	constructor() {
 		super();
 	}
 
 	/**
-	 * Loads a .obj file from a given filepath
+	 * Loads a .obj file from a given buffer
+	 * @override
+	 * @param {ArrayBuffer} fileBuffer file as a buffer
+	 * @param {THREE.Object3D} parent three.js object parent
+	 * @param {number} color color to tint the object with
+	 * @returns {Promise<undefined>}
 	 */
-	async load(fileBuffer, parent) {
-		const { OBJLoader } = await import(
-			'three/examples/jsm/loaders/OBJLoader.js'
-		);
-		this.objLoader = new OBJLoader();
+	async load(fileBuffer, parent, color) {
+		if (!this.#objLoader) {
+			const { OBJLoader } = await import(
+				'three/examples/jsm/loaders/OBJLoader.js'
+			);
+			this.#objLoader = new OBJLoader();
+		}
+		if (!this.#mergeBufferGeometries) {
+			const { mergeBufferGeometries } = await import(
+				'three/examples/jsm/utils/BufferGeometryUtils.js'
+			);
+			this.#mergeBufferGeometries = mergeBufferGeometries;
+		}
 
 		const uint8array = new Uint8Array(fileBuffer);
 		const decoder = new TextDecoder('utf8');
 		let stringData = decoder.decode(uint8array);
 
-		const objectGroup = this.objLoader.parse(stringData);
+		const objectGroup = this.#objLoader.parse(stringData);
 
-		const object = new THREE.Object3D();
+		const meshes = [];
 
-		objectGroup.children.forEach((child) => object.add(child));
+		objectGroup.children.forEach((child) => {
+			if (child.isMesh) {
+				meshes.push(child);
+			}
+		});
 
-		return object;
+		const mergedGeometry = this.#mergeBufferGeometries(
+			meshes.map((mesh) => mesh.geometry),
+		);
+
+		mergedGeometry.computeBoundingBox();
+		mergedGeometry.center();
+
+		const material = new THREE.MeshPhongMaterial({ color });
+		const mergedMesh = new THREE.Mesh(mergedGeometry, material);
+		mergedMesh.position.setScalar(0);
+
+		parent.add(mergedMesh);
 	}
 }
 
